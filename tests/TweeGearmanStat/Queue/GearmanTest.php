e16ce47cb99411a7156eb0a587099d52144275e2
Rostislav Mykhajliw <necromant2005@gmail.com>
2013-06-13 22:49:15 +0300

    Update GearmanTest.php

--- tests/TweeGearmanStat/Queue/GearmanTest.php
+++ tests/TweeGearmanStat/Queue/GearmanTest.php
@@ -13,9 +13,10 @@ class GearmanTest extends PHPUnit_Framework_TestCase
         $status = $statuses['default'];
         $this->assertContains(array('name' => 'test1', 'queue' => 0, 'running' => 0, 'workers' => 1), $status);
         $this->assertContains(array('name' => 'test2', 'queue' => 0, 'running' => 0, 'workers' => 1), $status);
-
-        # ---
-
+    }
+    
+    public function testConnectWithDefaultTimeout()
+    {
         $adapter = new Gearman(array(
             'default' => array('host' => '127.0.0.1', 'port' => 4730),
         ));

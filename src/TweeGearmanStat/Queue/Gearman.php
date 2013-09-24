764bec67eff99a705187c6bcec943f1e2a173a74
Brandon Woodmansee <brandon.woodmansee@hautelook.com>
2013-09-24 13:54:21 -0700

    Move declarations out of foreach; Eliminate unnecessary var

--- src/TweeGearmanStat/Queue/Gearman.php
+++ src/TweeGearmanStat/Queue/Gearman.php
@@ -51,12 +51,18 @@ class Gearman
         if (empty($this->servers)) {
             return false;
         }
+        $error = '';
+        $errno = 0;
+        $timeout = isset($options[self::OPTION_TIMEOUT]) ? $options[self::OPTION_TIMEOUT] : $this->defaultTimeout;
         foreach ($this->servers as $name => $options) {
-            $error = '';
-            $errno = 0;
-            $timeout = isset($options[self::OPTION_TIMEOUT]) ? $options[self::OPTION_TIMEOUT] : $this->defaultTimeout;
-            $socket  = fsockopen($options[self::OPTION_HOST], $options[self::OPTION_PORT], $error, $errno, $timeout );
-            $this->connections[$name] = $socket;
+            $this->connections[$name] =
+                fsockopen(
+                    $options[self::OPTION_HOST],
+                    $options[self::OPTION_PORT],
+                    $error,
+                    $errno,
+                    $timeout
+                );
         }
 
         return true;

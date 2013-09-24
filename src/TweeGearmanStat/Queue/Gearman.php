72770088a827508bab4f5a3001614da8d1ec2457
Brandon Woodmansee <brandon.woodmansee@hautelook.com>
2013-09-24 10:18:24 -0700

    Move fsockopen out of constructor

--- src/TweeGearmanStat/Queue/Gearman.php
+++ src/TweeGearmanStat/Queue/Gearman.php
@@ -10,21 +10,17 @@ class Gearman
     private $defaultTimeout = 5;
 
     protected $connections = array();
+    protected $servers = array();
 
     public function __construct(array $servers)
     {
-        foreach ($servers as $name => $options) {
-            $error = '';
-            $errno = 0;
-            $timeout = isset($options[self::OPTION_TIMEOUT]) ? $options[self::OPTION_TIMEOUT] : $this->defaultTimeout;
-            $socket  = fsockopen($options[self::OPTION_HOST], $options[self::OPTION_PORT], $error, $errno, $timeout );
-            $this->connections[$name] = $socket;
-        }
+        $this->servers = $servers;
     }
 
     public function status()
     {
         $responses = array();
+        $this->connect();
         foreach ($this->connections as $name => $socket) {
             $body = trim($this->command($socket, 'status'));
             $response = array();
@@ -45,5 +41,16 @@ class Gearman
             $response .= $line;
         }
         return $response;
+    }
+
+    protected function connect()
+    {
+        foreach ($this->servers as $name => $options) {
+            $error = '';
+            $errno = 0;
+            $timeout = isset($options[self::OPTION_TIMEOUT]) ? $options[self::OPTION_TIMEOUT] : $this->defaultTimeout;
+            $socket  = fsockopen($options[self::OPTION_HOST], $options[self::OPTION_PORT], $error, $errno, $timeout );
+            $this->connections[$name] = $socket;
+        }
     }
 }
